45d4dee7886b4807a261e698035f58f691d1807c
Sagar Mittal <sagar@sagarmittal.com>
2021-12-21 22:49:44 -0800

    Small simplification of 5b

--- src/bin/5.rs
+++ src/bin/5.rs
@@ -2,8 +2,6 @@ use regex::Regex;
 use std::io;
 use std::io::BufRead;
 
-type Coord = usize;
-
 fn run() -> io::Result<i32> {
     let mut grid = [[0; 1000]; 1000];
     let mut cnt = 0;
@@ -12,14 +10,15 @@ fn run() -> io::Result<i32> {
     for line in stdin.lock().lines() {
         let line = line.unwrap();
         let m = parser.captures(line.trim()).unwrap();
-        let mut x1: Coord = m[1].parse().unwrap();
-        let mut y1: Coord = m[2].parse().unwrap();
-        let x2: Coord = m[3].parse().unwrap();
-        let y2: Coord = m[4].parse().unwrap();
+        let mut x1: i32 = m[1].parse().unwrap();
+        let mut y1: i32 = m[2].parse().unwrap();
+        let x2: i32 = m[3].parse().unwrap();
+        let y2: i32 = m[4].parse().unwrap();
         println!("\"{}\" {} {} -- {} {}", line, x1, y1, x2, y2);
         loop {
-            grid[x1][y1] += 1;
-            if grid[x1][y1] == 2 {
+            let pos = &mut grid[x1 as usize][y1 as usize];
+            *pos += 1;
+            if *pos == 2 {
                 cnt += 1;
             }
 
@@ -27,16 +26,8 @@ fn run() -> io::Result<i32> {
                 break;
             }
 
-            if x1 < x2 {
-                x1 += 1;
-            } else if x1 > x2 {
-                x1 -= 1;
-            }
-            if y1 < y2 {
-                y1 += 1;
-            } else if y1 > y2 {
-                y1 -= 1;
-            }
+            x1 += (x2-x1).signum();
+            y1 += (y2-y1).signum();
         }
     }
     Ok(cnt)
